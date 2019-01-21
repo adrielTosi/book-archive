1b005d32d3f50b5d0e1e25ae458703a237102bdb
Adriel Tosi <adrielstosi@gmail.com>
2019-01-21 18:43:07 +0100

    Updates

---
+++ precache-manifest.10dd94f71d822887aad7581c17150275.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8f06080586e04d8b6a6",
+    "url": "/book-deploy/static/css/main.419e5c47.chunk.css"
+  },
+  {
+    "revision": "a8f06080586e04d8b6a6",
+    "url": "/book-deploy/static/js/main.a8f06080.chunk.js"
+  },
+  {
+    "revision": "94c1f2103c673748bdf8",
+    "url": "/book-deploy/static/js/1.94c1f210.chunk.js"
+  },
+  {
+    "revision": "e5406107bba2e3c417df",
+    "url": "/book-deploy/static/js/runtime~main.e5406107.js"
+  },
+  {
+    "revision": "bf645244e1f0e41cb1e5f765b6da95cf",
+    "url": "/book-deploy/index.html"
+  }
+];
